e1c6476a5e7b9e4e241c7811a6c35524d06dc3f2
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-03-25 21:51:53 -0500

    added bootstrap and components

--- Brewery Search App/client/src/Components/BreweryDisplay.js
+++ Brewery Search App/client/src/Components/BreweryDisplay.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import BreweryList from './BreweryList'
 import BreweryMap from './BreweryMap'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function BreweryDisplay() {
     return (
-        <div>
-            <BreweryList />
-            <BreweryMap />
-        </div>
+        <Container>
+            <Row>
+                <Col><BreweryList /></Col>
+                <Col><BreweryMap /></Col>
+            </Row>
+        </Container>
     )
 }
 
--- Brewery Search App/client/src/Components/SearchBar.js
+++ Brewery Search App/client/src/Components/SearchBar.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
 
 function SearchBar() {
     return (
-        <div>
-            <input></input>
-        </div>
+        <Container id="search-bar-container">
+            <Row id="search-bar-row">
+                <Form>
+                    <Form.Group>
+                        <Form.Control placeholder="Search Breweries"></Form.Control>
+                    </Form.Group>
+                </Form>
+                <Button id="search-bar-button">Search</Button>
+            </Row>
+        </Container>
     )
 }
 

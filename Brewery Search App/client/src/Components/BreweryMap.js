70558caeac622f10bf0b5a96e09337952e392e02
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-03-30 01:36:30 -0500

    added map as background

--- Brewery Search App/client/src/Components/BreweryMap.js
+++ Brewery Search App/client/src/Components/BreweryMap.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 function BreweryMap() {
+
+    const containerStyles = {
+        width: '100%',
+        height: '100%',
+    };
+
     return (
         <div id="brewerymap-container">
-            Brewery map place holder
+            <LoadScript
+                googleMapsApiKey={GOOGLE_API_KEY}
+            >
+                <GoogleMap
+                    mapContainerStyle={containerStyles}
+                    center={{ lat: 37.807739, lng: -122.2698881 }}
+                    zoom={8}
+                />
+            </LoadScript>
         </div>
     )
 }

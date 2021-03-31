f896c3693091039c30fdaf642ae3b05070fe9a8f
Eddie Cuellar <heriberto.cuellar1329@gmail.com>
2021-03-31 00:03:20 -0500

    fixed map key, and added a location marker

--- Brewery Search App/client/src/Components/BreweryMap.js
+++ Brewery Search App/client/src/Components/BreweryMap.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api'
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+import React, { useEffect } from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
 function BreweryMap() {
+    const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
     const containerStyles = {
         width: '100%',
         height: '100%',
     };
+
+    const locations = [
+        {
+            name: 'Location1',
+            location: {
+                lat: 37.807739,
+                lng: -122.2698881
+            }
+        }
+    ]
 
     return (
         <div id="brewerymap-container">
@@ -17,8 +27,15 @@ function BreweryMap() {
                 <GoogleMap
                     mapContainerStyle={containerStyles}
                     center={{ lat: 37.807739, lng: -122.2698881 }}
-                    zoom={8}
-                />
+                    zoom={8} >
+                    {
+                        locations.map(loc => {
+                            return (
+                                <Marker key={loc.name} position={loc.location} />
+                            )
+                        })
+                    }
+                </GoogleMap>
             </LoadScript>
         </div>
     )
